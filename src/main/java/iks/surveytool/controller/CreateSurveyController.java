5eccf531d569414d538678f50cc5a0f430d5ce23
pamei07 <p.meinhard@iks-gmbh.com>
2021-10-06 10:15:53 +0200

    Add new survey to model in GetMapping

--- src/main/java/iks/surveytool/controller/CreateSurveyController.java
+++ src/main/java/iks/surveytool/controller/CreateSurveyController.java
@@ -1,7 +1,9 @@
 package iks.surveytool.controller;
 
+import iks.surveytool.entities.Survey;
 import iks.surveytool.services.SurveyService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,8 +14,8 @@ public class CreateSurveyController {
     SurveyService surveyService = new SurveyService();
 
     @GetMapping("")
-    public String start() {
-
+    public String surveyForm(Model model) {
+        model.addAttribute("newSurvey", new Survey());
         return "createSurvey";
     }
 }
