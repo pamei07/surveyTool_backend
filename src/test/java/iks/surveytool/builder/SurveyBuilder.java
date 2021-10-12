3b585ed9df948513301f935855601c6dfa946bd1
pamei07 <p.meinhard@iks-gmbh.com>
2021-10-12 14:07:21 +0200

    Add SurveyBuilder for testing

---
+++ src/test/java/iks/surveytool/builder/SurveyBuilder.java
@@ -0,0 +1,96 @@
+package iks.surveytool.builder;
+
+import iks.surveytool.entities.QuestionGroup;
+import iks.surveytool.entities.Survey;
+import iks.surveytool.entities.User;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SurveyBuilder {
+
+    Long id = 1L;
+    String name = "Test Umfrage";
+    String description = "Default Beschreibung";
+    LocalDateTime startDate = LocalDateTime.of(2050, 1, 1, 12, 0);
+    LocalDateTime endDate = startDate.plusWeeks(1L);
+    boolean open = false;
+    String accessID = "Test ID";
+    String link = "Eine URL";
+    User user = new User();
+    List<QuestionGroup> questionGroups = new ArrayList<>();
+
+    public SurveyBuilder setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public SurveyBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public SurveyBuilder setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public SurveyBuilder setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+        return this;
+    }
+
+    public SurveyBuilder setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+        return this;
+    }
+
+    public SurveyBuilder setOpen(boolean open) {
+        this.open = open;
+        return this;
+    }
+
+    public SurveyBuilder setAccessID(String accessID) {
+        this.accessID = accessID;
+        return this;
+    }
+
+    public SurveyBuilder setLink(String link) {
+        this.link = link;
+        return this;
+    }
+
+    public SurveyBuilder setUser(User user) {
+        this.user = user;
+        return this;
+    }
+
+    public SurveyBuilder setQuestionGroups(List<QuestionGroup> questionGroups) {
+        this.questionGroups = questionGroups;
+        return this;
+    }
+
+    public SurveyBuilder addQuestionGroup(String title) {
+        QuestionGroup questionGroup = new QuestionGroup();
+        questionGroup.setTitle(title);
+        this.questionGroups.add(questionGroup);
+
+        return this;
+    }
+
+    public Survey build() {
+        Survey newSurvey = new Survey();
+        newSurvey.setId(id);
+        newSurvey.setName(name);
+        newSurvey.setDescription(description);
+        newSurvey.setStartDate(startDate);
+        newSurvey.setEndDate(endDate);
+        newSurvey.setOpen(open);
+        newSurvey.setAccessID(accessID);
+        newSurvey.setLink(link);
+        newSurvey.setUser(user);
+        newSurvey.setQuestionGroups(questionGroups);
+        return newSurvey;
+    }
+}
