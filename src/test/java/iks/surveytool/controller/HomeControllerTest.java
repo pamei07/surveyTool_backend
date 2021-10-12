13e30e68aa06782bb2237320d250ffefac29c590
pamei07 <p.meinhard@iks-gmbh.com>
2021-10-12 09:07:45 +0200

    Test index()-method in HomeController

---
+++ src/test/java/iks/surveytool/controller/HomeControllerTest.java
@@ -0,0 +1,27 @@
+package iks.surveytool.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(HomeController.class)
+@DisplayName("Testing HomeController")
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("Test index()")
+    void index() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+}
