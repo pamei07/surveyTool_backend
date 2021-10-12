862e8929f5898268a55250e15a55d06ba48a33b0
pamei07 <p.meinhard@iks-gmbh.com>
2021-10-12 08:53:53 +0200

    Rename home.html to index.html

--- src/main/java/iks/surveytool/controller/HomeController.java
+++ src/main/java/iks/surveytool/controller/HomeController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeController {
     @GetMapping("")
-    public String home() {
+    public String index() {
 
-        return "home";
+        return "index";
     }
 }
