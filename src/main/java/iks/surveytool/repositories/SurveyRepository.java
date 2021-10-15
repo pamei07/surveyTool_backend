c96206c93c20bb90f3da27c1ecb78fd1b61e1aa1
pamei07 <p.meinhard@iks-gmbh.com>
2021-10-15 15:30:44 +0200

    Add query to find Survey by UUID

--- src/main/java/iks/surveytool/repositories/SurveyRepository.java
+++ src/main/java/iks/surveytool/repositories/SurveyRepository.java
@@ -4,7 +4,10 @@ import iks.surveytool.entities.Survey;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface SurveyRepository extends JpaRepository<Survey, Long> {
     Optional<Survey> findSurveyById(Long id);
+
+    Optional<Survey> findSurveyByUuid(UUID uuid);
 }
