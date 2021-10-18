bce992b53ef6c28d9f5c865852bce40dc0ed21f7
pamei07 <p.meinhard@iks-gmbh.com>
2021-10-18 14:48:11 +0200

    Add survey model

---
+++ src/main/resources/frontend/angularclient/src/app/model/survey.ts
@@ -0,0 +1,7 @@
+export class Survey {
+  id: string | undefined;
+  name: string | undefined;
+  description: string | undefined;
+  startDate: string | undefined;
+  endDate: string | undefined;
+}
