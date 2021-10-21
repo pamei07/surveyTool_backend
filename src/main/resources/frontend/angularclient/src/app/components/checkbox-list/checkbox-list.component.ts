838004b2066b4d967e22b9dd62f894174b79947e
pamei07 <p.meinhard@iks-gmbh.com>
2021-10-21 12:30:45 +0200

    Extract checkbox form into own component

--- src/main/resources/frontend/angularclient/src/app/components/checkbox-list/checkbox-list.component.ts
+++ src/main/resources/frontend/angularclient/src/app/components/checkbox-list/checkbox-list.component.ts
@@ -1,6 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Survey} from "../../model/survey";
-import {Checkbox} from "../../model/checkbox";
 
 @Component({
   selector: 'checkbox-list',
@@ -9,29 +8,13 @@ import {Checkbox} from "../../model/checkbox";
 
 export class CheckboxListComponent implements OnInit {
 
-  checkbox: Checkbox;
   @Input() indexQuestion!: number;
   @Input() indexQuestionGroup!: number;
   @Input() survey!: Survey;
 
   constructor() {
-    this.checkbox = new Checkbox();
   }
 
   ngOnInit() {
-  }
-
-  onSubmit() {
-    console.log(this.checkbox);
-
-    this.survey
-      .questionGroups![this.indexQuestionGroup]
-      .questions![this.indexQuestion]
-      .checkboxGroup!
-      .checkboxes!.push(this.checkbox);
-
-    sessionStorage.setItem('newSurvey', JSON.stringify(this.survey));
-
-    this.checkbox = new Checkbox();
   }
 }
