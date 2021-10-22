be5f246b82db5b7f2d1eb18b02234e18c193fca9
pamei07 <p.meinhard@iks-gmbh.com>
2021-10-22 12:29:44 +0200

    Move Angular app into own repository

--- src/main/resources/frontend/angularclient/src/app/model/checkbox.ts
+++
@@ -1,9 +0,0 @@
-import {CheckboxGroup} from "./checkbox-group";
-
-export class Checkbox {
-  id: number | undefined;
-  text: string | undefined;
-  hasTextField: boolean | undefined;
-  checkboxGroup: CheckboxGroup | undefined;
-  answers: Object[] | undefined
-}
