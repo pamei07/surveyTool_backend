c436cce6cc71a3742c254ff7007de757bd1c2747
pamei07 <p.meinhard@iks-gmbh.com>
2021-10-28 12:38:34 +0200

    Add user to answer before saving

--- src/main/java/iks/surveytool/services/AnswerService.java
+++ src/main/java/iks/surveytool/services/AnswerService.java
@@ -3,9 +3,11 @@ package iks.surveytool.services;
 import iks.surveytool.entities.Answer;
 import iks.surveytool.entities.Checkbox;
 import iks.surveytool.entities.Question;
+import iks.surveytool.entities.User;
 import iks.surveytool.repositories.AnswerRepository;
 import iks.surveytool.repositories.CheckboxRepository;
 import iks.surveytool.repositories.QuestionRepository;
+import iks.surveytool.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,10 +20,12 @@ public class AnswerService {
     private final AnswerRepository answerRepository;
     private final QuestionRepository questionRepository;
     private final CheckboxRepository checkboxRepository;
+    private final UserRepository userRepository;
 
     public void saveListOfAnswers(List<Answer> answerList) {
-        // Need to fetch question from db for hibernate to recognize it
         for (Answer answer : answerList) {
+            // Need to fetch question from db for hibernate to recognize it
+
             Long questionID = answer.getQuestion().getId();
             Optional<Question> questionOptional = questionRepository.findById(questionID);
 
@@ -38,7 +42,15 @@ public class AnswerService {
                         answer.setCheckbox(checkbox);
                     }
                 }
+            }
 
+            // Need to fetch user from db for hibernate to recognize it
+            Long userID = answer.getUser().getId();
+            Optional<User> userOptional = userRepository.findById(userID);
+
+            if (userOptional.isPresent()) {
+                User user = userOptional.get();
+                answer.setUser(user);
             }
         }
         answerRepository.saveAll(answerList);
