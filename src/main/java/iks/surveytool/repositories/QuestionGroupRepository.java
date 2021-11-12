e83238256f392cf89c4cae086953229392da64a8
pamei07 <p.meinhard@iks-gmbh.com>
2021-11-12 09:37:58 +0100

    Remove unused method

--- src/main/java/iks/surveytool/repositories/QuestionGroupRepository.java
+++ src/main/java/iks/surveytool/repositories/QuestionGroupRepository.java
@@ -4,5 +4,5 @@ import iks.surveytool.entities.QuestionGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionGroupRepository extends JpaRepository<QuestionGroup, Long> {
-    QuestionGroup findQuestionGroupById(Long id);
+
 }
