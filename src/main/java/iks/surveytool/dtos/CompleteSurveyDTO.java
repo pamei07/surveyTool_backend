43b99d79ee598e8b7671fbf91effbc56a78b37e1
pamei07 <p.meinhard@iks-gmbh.com>
2021-11-17 12:53:13 +0100

    Match model in frontend regarding user in survey object

--- src/main/java/iks/surveytool/dtos/CompleteSurveyDTO.java
+++ src/main/java/iks/surveytool/dtos/CompleteSurveyDTO.java
@@ -14,8 +14,8 @@ public class CompleteSurveyDTO extends SurveyOverviewDTO {
 
     private List<QuestionGroupDTO> questionGroups;
 
-    public CompleteSurveyDTO(Long id, String name, String description, LocalDateTime startDate, LocalDateTime endDate, boolean open, String accessID, UUID uuid, UserDTO user, List<QuestionGroupDTO> questionGroups) {
-        super(id, name, description, startDate, endDate, open, accessID, uuid, user);
+    public CompleteSurveyDTO(Long id, String name, String description, LocalDateTime startDate, LocalDateTime endDate, boolean open, String accessID, UUID uuid, Long userID, List<QuestionGroupDTO> questionGroups) {
+        super(id, name, description, startDate, endDate, open, accessID, uuid, userID);
         this.questionGroups = questionGroups;
     }
 }
