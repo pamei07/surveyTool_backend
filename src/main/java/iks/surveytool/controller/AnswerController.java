1248f5a19d1b97a89eb28aba3ce41dd4f2937e4a
pamei07 <p.meinhard@iks-gmbh.com>
2021-11-23 09:20:24 +0100

    Validate list of answers on submit

--- src/main/java/iks/surveytool/controller/AnswerController.java
+++ src/main/java/iks/surveytool/controller/AnswerController.java
@@ -3,6 +3,7 @@ package iks.surveytool.controller;
 import iks.surveytool.dtos.AnswerDTO;
 import iks.surveytool.entities.Answer;
 import iks.surveytool.services.AnswerService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -25,9 +26,13 @@ public class AnswerController {
     public ResponseEntity<List<AnswerDTO>> postAnswers(@RequestBody AnswerDTO[] answerDTOs) {
         List<AnswerDTO> answerDTOList = Arrays.asList(answerDTOs);
         List<Answer> answerList = answerService.createAnswersFromDtos(answerDTOList);
-        List<Answer> savedAnswers = answerService.saveListOfAnswers(answerList);
-        List<AnswerDTO> savedAnswerDTOs = answerService.createAnswerDtos(savedAnswers);
-        return ResponseEntity.ok(savedAnswerDTOs);
+        if (answerService.validate(answerList)) {
+            List<Answer> savedAnswers = answerService.saveListOfAnswers(answerList);
+            List<AnswerDTO> savedAnswerDTOs = answerService.createAnswerDtos(savedAnswers);
+            return ResponseEntity.ok(savedAnswerDTOs);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+        }
     }
 
     @GetMapping("/question")
--- src/main/java/iks/surveytool/services/AnswerService.java
+++ src/main/java/iks/surveytool/services/AnswerService.java
@@ -3,6 +3,9 @@ package iks.surveytool.services;
 import iks.surveytool.components.Mapper;
 import iks.surveytool.dtos.AnswerDTO;
 import iks.surveytool.entities.Answer;
+import iks.surveytool.entities.Checkbox;
+import iks.surveytool.entities.Question;
+import iks.surveytool.entities.User;
 import iks.surveytool.repositories.AnswerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -40,5 +43,38 @@ public class AnswerService {
 
     public List<Answer> saveListOfAnswers(List<Answer> answerList) {
         return answerRepository.saveAll(answerList);
+    }
+
+    public boolean validate(List<Answer> answerList) {
+        for (Answer answer : answerList) {
+            if (!validateAnswer(answer)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean validateAnswer(Answer answer) {
+        User user = answer.getUser();
+        Question question = answer.getQuestion();
+        if (user == null || question == null) {
+            return false;
+        } else if (question.isHasCheckbox()) {
+            Checkbox checkbox = answer.getCheckbox();
+            if (checkbox == null) {
+                return false;
+            } else if (checkbox.isHasTextField()) {
+                String text = answer.getText();
+                if (text == null) {
+                    return false;
+                }
+            }
+        } else if (question.isRequired() && !question.isHasCheckbox()) {
+            String text = answer.getText();
+            if (text == null || text.equals("")) {
+                return false;
+            }
+        }
+        return true;
     }
 }
