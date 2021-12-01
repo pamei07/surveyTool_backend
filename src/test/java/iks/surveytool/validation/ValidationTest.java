34cb84af1568e8b8797fa51751228682287cd640
pamei07 <p.meinhard@iks-gmbh.com>
2021-12-01 15:25:09 +0100

    Rename SurveyServiceTest

--- src/test/java/iks/surveytool/validation/ValidationTest.java
+++ src/test/java/iks/surveytool/validation/ValidationTest.java
@@ -1,13 +1,10 @@
-package iks.surveytool.services;
+package iks.surveytool.validation;
 
 import iks.surveytool.builder.*;
 import iks.surveytool.entities.*;
-import iks.surveytool.repositories.SurveyRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
@@ -15,29 +12,10 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-@DisplayName("Testing SurveyService")
-class SurveyServiceTest {
-
-    @Mock
-    private SurveyRepository surveyRepository;
-
-    @InjectMocks
-    private SurveyService surveyService;
-
-    @Test
-    @DisplayName("Adding Survey")
-    void addSurvey() {
-        Survey survey = new SurveyBuilder()
-                .createSurveyWithDefaultDate(1L, "Test Umfrage");
-        when(surveyRepository.save(survey)).thenReturn(survey);
-
-        surveyService.saveSurvey(survey);
-
-        verify(surveyRepository, times(1)).save(survey);
-    }
+@DisplayName("Testing validation of entities")
+class ValidationTest {
 
     @Test
     @DisplayName("Failed validation - Survey missing QuestionGroups")
