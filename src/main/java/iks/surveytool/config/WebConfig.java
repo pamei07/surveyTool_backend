36a982674f260dfb619fa93e6452353aa99767a6
pamei07 <p.meinhard@iks-gmbh.com>
2021-12-15 11:39:46 +0100

    Fix CORS policy

--- src/main/java/iks/surveytool/config/WebConfig.java
+++ src/main/java/iks/surveytool/config/WebConfig.java
@@ -10,10 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .allowedOrigins("https://umfragetool.herokuapp.com/")
-                .allowedHeaders("*");
-        corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200/")
+                .allowedOrigins("http://localhost:4200/", "https://umfragetool.herokuapp.com/")
                 .allowedHeaders("*");
     }
 }
