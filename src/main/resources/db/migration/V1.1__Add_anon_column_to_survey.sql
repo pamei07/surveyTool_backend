80ea08f7e04e1eec0f74db71d726d98f876b9c92
pamei07 <p.meinhard@iks-gmbh.com>
2021-12-20 14:46:39 +0100

    Make V1.1 work on already filled db

--- src/main/resources/db/migration/V1.1__Add_anon_column_to_survey.sql
+++ src/main/resources/db/migration/V1.1__Add_anon_column_to_survey.sql
@@ -1,2 +1,9 @@
 ALTER TABLE survey
-    ADD COLUMN anonymousParticipation BOOLEAN NOT NULL;
+    ADD COLUMN anonymousParticipation BOOLEAN NULL;
+
+UPDATE survey
+SET anonymousParticipation = FALSE
+WHERE survey.anonymousParticipation IS NULL;
+
+ALTER TABLE survey
+    ALTER COLUMN anonymousParticipation SET NOT NULL;
