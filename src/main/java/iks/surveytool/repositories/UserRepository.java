5ff9e9a6a2d6633649960d4183cda926eba0f11d
pamei07 <p.meinhard@iks-gmbh.com>
2022-02-16 12:21:15 +0100

    Optimize imports

--- src/main/java/iks/surveytool/repositories/UserRepository.java
+++ src/main/java/iks/surveytool/repositories/UserRepository.java
@@ -2,10 +2,7 @@ package iks.surveytool.repositories;
 
 import iks.surveytool.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
